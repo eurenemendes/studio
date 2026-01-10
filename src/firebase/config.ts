bee38fa46a963a5439582f483387a15c4bf64a9e
Rene mendes <Renemendes1997@gmail.com>
2026-01-10 14:42:52 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4145782589-74a32",
-  "appId": "1:1006462699084:web:26321fa39e8115998d96e9",
-  "apiKey": "AIzaSyBDYK9KqnLwmnSrozvqYKwXzkwKyDAq-tA",
-  "authDomain": "studio-4145782589-74a32.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1006462699084"
+  "projectId": "ecofeira-a05e8",
+  "appId": "1:634882425514:web:03c9b3252a974b53ce7009",
+  "apiKey": "AIzaSyDFf-rM_SAdBBDj2yBGq42A2z4F4wz-eCI",
+  "authDomain": "ecofeira-a05e8.firebaseapp.com",
+  "storageBucket": "ecofeira-a05e8.appspot.com",
+  "messagingSenderId": "634882425514"
 };
